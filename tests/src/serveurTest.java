221580f64844cf3033c06d446db685f0bb17ab58
Haelia <sarahwissocq@free.fr>
2016-02-14 18:27:50 +0100

    todo : tests

---
+++ tests/src/serveurTest.java
@@ -0,0 +1,14 @@
+package src;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class serveurTest {
+
+	@Test
+	public void test() {
+		mock();
+		fail("Not yet implemented");
+	}
+
+}
