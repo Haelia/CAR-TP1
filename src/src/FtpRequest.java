6fe23ab894107cdae1481e59c1e933ad6d62cfa4
Kaendan <tristan.camus@gmail.com>
2016-02-25 01:22:58 +0100

    Added sendMessage method

--- src/src/FtpRequest.java
+++ src/src/FtpRequest.java
@@ -16,15 +16,30 @@ import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FtpRequest extends Thread {
 
-	private static String cmd_sans_param[] = { Constantes.CMD_QUIT, Constantes.CMD_LIST, Constantes.CMD_SYST,
-			Constantes.CMD_EPSV, Constantes.CMD_PWD, Constantes.CMD_CDUP, Constantes.CMD_PASV };
+	public static final Map<String, Boolean> commandes;
 
-	private static String cmd_avec_param[] = { Constantes.CMD_USER, Constantes.CMD_PASS, Constantes.CMD_RETR,
-			Constantes.CMD_STOR, Constantes.CMD_EPRT, Constantes.CMD_PORT, Constantes.CMD_CWD, };
+	static {
+		commandes = new HashMap<>();
+		commandes.put(Constantes.CMD_USER, true);
+		commandes.put(Constantes.CMD_PASS, true);
+		commandes.put(Constantes.CMD_QUIT, false);
+		commandes.put(Constantes.CMD_SYST, false);
+		commandes.put(Constantes.CMD_PORT, true);
+		commandes.put(Constantes.CMD_PASV, false);
+		commandes.put(Constantes.CMD_EPRT, true);
+		commandes.put(Constantes.CMD_EPSV, false);
+		commandes.put(Constantes.CMD_LIST, false);
+		commandes.put(Constantes.CMD_RETR, true);
+		commandes.put(Constantes.CMD_STOR, true);
+		commandes.put(Constantes.CMD_PWD, false);
+		commandes.put(Constantes.CMD_CWD, true);
+		commandes.put(Constantes.CMD_CDUP, false);
+	}
 
 	/**
 	 * Le socket de communication
@@ -107,6 +122,7 @@ public class FtpRequest extends Thread {
 	 */
 	public void processRequest() throws IOException {
 		String cmd = null;
+		String param = null;
 
 		do {
 			// Reception d'une requête sur l'entrée du socket
@@ -114,9 +130,11 @@ public class FtpRequest extends Thread {
 
 			System.out.println("Request: " + request);
 
-			int index = request.indexOf(" ");
-			cmd = index != -1 ? request.substring(0, index) : request;
-			String param = index != -1 ? request.substring(index + 1) : null;
+			if (request != null) {
+				int index = request.indexOf(" ");
+				cmd = index != -1 ? request.substring(0, index) : request;
+				param = index != -1 ? request.substring(index + 1) : null;
+			}
 
 			// Vérification de la validité de la commande : Elle doit exister et
 			// avoir un nombre de paramêtre correcte. L'utilisateur doit aussi
@@ -181,12 +199,24 @@ public class FtpRequest extends Thread {
 					break;
 				}
 			} else {
-				cmd = null;
+				cmd = null; // évite de sortir de la boucle quand la commande
+							// non valide est QUIT
 			}
 
 		} while (!Constantes.CMD_QUIT.equals(cmd));
 		// processRequest continue la reception de requêtes tant que la commande
 		// QUIT n'est pas appelée
+	}
+
+	/**
+	 * Méthode envoyant un message au client sur l'ouput du socket
+	 * 
+	 * @param msg
+	 *            le message à envoyer
+	 */
+	public void sendMessage(String msg) {
+		this.output.println(msg);
+		this.output.flush();
 	}
 
 	/**
@@ -204,8 +234,8 @@ public class FtpRequest extends Thread {
 		}
 		if (requiertAuthenfication(cmd)) {
 			if (!this.estAuthentifie()) {
-				this.output.println(Constantes.CODE_NON_AUTH + " " + Constantes.MSG_NON_AUTH);
-				this.output.flush();
+				String msg = Constantes.CODE_NON_AUTH + " " + Constantes.MSG_NON_AUTH;
+				this.sendMessage(msg);
 				return false;
 			}
 		}
@@ -222,15 +252,19 @@ public class FtpRequest extends Thread {
 	 * @return true si le nombre de paramètre est valide, false sinon
 	 */
 	public boolean estValideParametre(String cmd, boolean param) {
-		if (param && Arrays.asList(FtpRequest.cmd_avec_param).contains(cmd)) {
-			return true;
-
-		} else if (!param && Arrays.asList(FtpRequest.cmd_sans_param).contains(cmd)) {
-			return true;
+		if (!commandes.containsKey(cmd)) {
+			String msg = Constantes.CODE_CMD_INVALIDE + " " + Constantes.MSG_CMD_INVALIDE;
+			this.sendMessage(msg);
+			return false;
 		}
-		this.output.println(Constantes.CODE_PARAM_INVALIDE + " " + Constantes.MSG_PARAM_INVALIDE);
-		this.output.flush();
-		return false;
+
+		if (!commandes.get(cmd).equals(param)) {
+			String msg = Constantes.CODE_PARAM_INVALIDE + " " + Constantes.MSG_PARAM_INVALIDE;
+			this.sendMessage(msg);
+			return false;
+		}
+
+		return true;
 	}
 
 	/**
@@ -272,11 +306,11 @@ public class FtpRequest extends Thread {
 		this.authentifie = false;
 		if (Serveur.utilisateurs.containsKey(user)) {
 			this.identifiant = user;
-			this.output.println(Constantes.CODE_ATTENTE_MDP + " " + Constantes.MSG_ATTENTE_MDP);
-			this.output.flush();
+			String msg = Constantes.CODE_ATTENTE_MDP + " " + Constantes.MSG_ATTENTE_MDP;
+			this.sendMessage(msg);
 		} else {
-			this.output.println(Constantes.CODE_AUTH_ECHOUE + " " + Constantes.MSG_AUTH_ECHOUE);
-			this.output.flush();
+			String msg = Constantes.CODE_AUTH_ECHOUE + " " + Constantes.MSG_AUTH_ECHOUE;
+			this.sendMessage(msg);
 		}
 
 	}
@@ -290,17 +324,17 @@ public class FtpRequest extends Thread {
 	 */
 	public boolean processPASS(String mdp) {
 		if (identifiant == "") {
-			this.output.println(Constantes.CODE_AUTH_ECHOUE + " " + Constantes.MSG_SAISIR_IDENTIFIANT);
-			this.output.flush();
+			String msg = Constantes.CODE_AUTH_ECHOUE + " " + Constantes.MSG_SAISIR_IDENTIFIANT;
+			this.sendMessage(msg);
 		} else {
 			if (Serveur.utilisateurs.get(this.identifiant).equals(mdp)) {
 				this.authentifie = true;
-				this.output.println(Constantes.CODE_AUTH_REUSSIE + " " + Constantes.MSG_AUTH_REUSSIE);
-				this.output.flush();
+				String msg = Constantes.CODE_AUTH_REUSSIE + " " + Constantes.MSG_AUTH_REUSSIE;
+				this.sendMessage(msg);
 				return true;
 			} else {
-				this.output.println(Constantes.CODE_AUTH_ECHOUE + " " + Constantes.MSG_AUTH_ECHOUE);
-				this.output.flush();
+				String msg = Constantes.CODE_AUTH_ECHOUE + " " + Constantes.MSG_AUTH_ECHOUE;
+				this.sendMessage(msg);
 			}
 		}
 		return false;
@@ -312,8 +346,8 @@ public class FtpRequest extends Thread {
 	 * @throws IOException
 	 */
 	public void processQUIT() throws IOException {
-		this.output.println(Constantes.CODE_DECONNEXION + " " + Constantes.MSG_DECONNEXION);
-		this.output.flush();
+		String msg = Constantes.CODE_DECONNEXION + " " + Constantes.MSG_DECONNEXION;
+		this.sendMessage(msg);
 		this.socket.close();
 	}
 
@@ -323,8 +357,8 @@ public class FtpRequest extends Thread {
 	 * @throws IOException
 	 */
 	public void processSYST() throws IOException {
-		this.output.println(Constantes.CODE_SYST + " " + Constantes.MSG_SYST);
-		this.output.flush();
+		String msg = Constantes.CODE_SYST + " " + Constantes.MSG_SYST;
+		this.sendMessage(msg);
 	}
 
 	/**
@@ -339,8 +373,8 @@ public class FtpRequest extends Thread {
 		int port = Integer.parseInt(s[4]) * 256 + Integer.parseInt(s[5]);
 
 		this.socketDonnees = new Socket(this.adresse, port);
-		this.output.println(Constantes.CODE_SERVICE_OK);
-		this.output.flush();
+		String msg = String.valueOf(Constantes.CODE_SERVICE_OK);
+		this.sendMessage(msg);
 	}
 
 	/**
@@ -356,8 +390,8 @@ public class FtpRequest extends Thread {
 			adresse += b + ",";
 		}
 		int port = serveur.getLocalPort();
-		this.output.println("227 Entering Passive Mode " + "(" + adresse + port / 256 + "," + port % 256 + ")");
-		this.output.flush();
+		String msg = "227 Entering Passive Mode " + "(" + adresse + port / 256 + "," + port % 256 + ")";
+		this.sendMessage(msg);
 		this.socketDonnees = serveur.accept();
 	}
 
@@ -373,8 +407,8 @@ public class FtpRequest extends Thread {
 		int port = Integer.parseInt(s[3]);
 
 		this.socketDonnees = new Socket(this.adresse, port);
-		this.output.println(Constantes.CODE_SERVICE_OK);
-		this.output.flush();
+		String msg = String.valueOf(Constantes.CODE_SERVICE_OK);
+		this.sendMessage(msg);
 	}
 
 	/**
@@ -386,8 +420,9 @@ public class FtpRequest extends Thread {
 		serveur = new ServerSocket(0);
 		int port = serveur.getLocalPort();
 
-		this.output.println("229 Entering Extended Passive Mode (|||" + port + "|)");
-		this.output.flush();
+		String msg = "229 Entering Extended Passive Mode (|||" + port + "|)";
+		this.sendMessage(msg);
+
 		this.socketDonnees = serveur.accept();
 	}
 
@@ -398,8 +433,8 @@ public class FtpRequest extends Thread {
 	 */
 	public boolean processLIST() throws IOException {
 		if (socketDonnees == null) {
-			this.output.println(Constantes.CODE_ERREUR_DONNEES + " " + Constantes.MSG_AUCUN_SOCKET_DONNEES);
-			this.output.flush();
+			String msg = Constantes.CODE_ERREUR_DONNEES + " " + Constantes.MSG_AUCUN_SOCKET_DONNEES;
+			this.sendMessage(msg);
 			return false;
 		}
 
@@ -410,8 +445,8 @@ public class FtpRequest extends Thread {
 			liste += fichier.getName() + "\n";
 		}
 
-		this.output.println(Constantes.CODE_LIST);
-		this.output.flush();
+		String msg = String.valueOf(Constantes.CODE_LIST);
+		this.sendMessage(msg);
 
 		OutputStreamWriter outputWriterData = new OutputStreamWriter(socketDonnees.getOutputStream());
 		outputWriterData.write(liste);
@@ -424,8 +459,8 @@ public class FtpRequest extends Thread {
 			this.serveur = null;
 		}
 
-		this.output.println(Constantes.CODE_226_LIST);
-		this.output.flush();
+		msg = String.valueOf(Constantes.CODE_226_LIST);
+		this.sendMessage(msg);
 		return true;
 	}
 
@@ -438,14 +473,14 @@ public class FtpRequest extends Thread {
 	 */
 	public boolean processRETR(String filename) {
 		if (socketDonnees == null) {
-			this.output.println(Constantes.CODE_ERREUR_DONNEES + " " + Constantes.MSG_AUCUN_SOCKET_DONNEES);
-			this.output.flush();
+			String msg = Constantes.CODE_ERREUR_DONNEES + " " + Constantes.MSG_AUCUN_SOCKET_DONNEES;
+			this.sendMessage(msg);
 			return false;
 		}
 
 		try {
-			this.output.println(Constantes.CODE_LIST);
-			this.output.flush();
+			String msg = String.valueOf(Constantes.CODE_LIST);
+			this.sendMessage(msg);
 			String path = this.repertoire.toPath().toAbsolutePath().toString() + "/" + filename;
 			Path target = Paths.get(path);
 
@@ -460,12 +495,12 @@ public class FtpRequest extends Thread {
 				this.serveur = null;
 			}
 		} catch (Exception e) {
-			this.output.println(Constantes.CODE_TRANSFERT_ECHOUE + " " + Constantes.MSG_TRANSFERT_ECHOUE);
-			this.output.flush();
+			String msg = Constantes.CODE_TRANSFERT_ECHOUE + " " + Constantes.MSG_TRANSFERT_ECHOUE;
+			this.sendMessage(msg);
 		}
 
-		this.output.println(Constantes.CODE_TRANSFERT_REUSSI + " " + Constantes.MSG_TRANSFERT_REUSSI);
-		this.output.flush();
+		String msg = Constantes.CODE_TRANSFERT_REUSSI + " " + Constantes.MSG_TRANSFERT_REUSSI;
+		this.sendMessage(msg);
 		return true;
 	}
 
@@ -478,14 +513,14 @@ public class FtpRequest extends Thread {
 	 */
 	public boolean processSTOR(String filename) {
 		if (socketDonnees == null) {
-			this.output.println(Constantes.CODE_ERREUR_DONNEES + " " + Constantes.MSG_AUCUN_SOCKET_DONNEES);
-			this.output.flush();
+			String msg = Constantes.CODE_ERREUR_DONNEES + " " + Constantes.MSG_AUCUN_SOCKET_DONNEES;
+			this.sendMessage(msg);
 			return false;
 		}
 
 		try {
-			this.output.println(Constantes.CODE_LIST);
-			this.output.flush();
+			String msg = String.valueOf(Constantes.CODE_LIST);
+			this.sendMessage(msg);
 			InputStream in = this.socketDonnees.getInputStream();
 
 			String path = this.repertoire.toPath().toAbsolutePath().toString() + "/" + filename;
@@ -500,12 +535,12 @@ public class FtpRequest extends Thread {
 				this.serveur = null;
 			}
 		} catch (IOException e) {
-			this.output.println(Constantes.CODE_TRANSFERT_ECHOUE + " " + Constantes.MSG_TRANSFERT_ECHOUE);
-			this.output.flush();
+			String msg = Constantes.CODE_TRANSFERT_ECHOUE + " " + Constantes.MSG_TRANSFERT_ECHOUE;
+			this.sendMessage(msg);
 		}
 
-		this.output.println(Constantes.CODE_TRANSFERT_REUSSI + " " + Constantes.MSG_TRANSFERT_REUSSI);
-		this.output.flush();
+		String msg = Constantes.CODE_TRANSFERT_REUSSI + " " + Constantes.MSG_TRANSFERT_REUSSI;
+		this.sendMessage(msg);
 		return true;
 	}
 
@@ -513,8 +548,8 @@ public class FtpRequest extends Thread {
 	 * Méthode éxécutant la commande PWD
 	 */
 	public void processPWD() {
-		this.output.println(Constantes.CODE_257_PWD + " " + this.repertoire.getAbsolutePath());
-		this.output.flush();
+		String msg = Constantes.CODE_257_PWD + " " + this.repertoire.getAbsolutePath();
+		this.sendMessage(msg);
 	}
 
 	/**
@@ -529,11 +564,11 @@ public class FtpRequest extends Thread {
 
 		if (Files.exists(path, LinkOption.NOFOLLOW_LINKS)) {
 			this.repertoire = new File(path.toString());
-			this.output.println(Constantes.CODE_FILEOP_COMPLETED + " " + this.repertoire.getAbsolutePath());
-			this.output.flush();
+			String msg = Constantes.CODE_FILEOP_COMPLETED + " " + this.repertoire.getAbsolutePath();
+			this.sendMessage(msg);
 		} else {
-			this.output.println(Constantes.CODE_REQUEST_NO_EXECUTED + " " + Constantes.MSG_NO_SUCH_FOLDER);
-			this.output.flush();
+			String msg = Constantes.CODE_REQUEST_NO_EXECUTED + " " + Constantes.MSG_NO_SUCH_FOLDER;
+			this.sendMessage(msg);
 		}
 	}
 
@@ -552,11 +587,11 @@ public class FtpRequest extends Thread {
 
 		if (Files.exists(path, LinkOption.NOFOLLOW_LINKS)) {
 			this.repertoire = new File(path.toString());
-			this.output.println(Constantes.CODE_FILEOP_COMPLETED + " " + this.repertoire.getAbsolutePath());
-			this.output.flush();
+			String msg = Constantes.CODE_FILEOP_COMPLETED + " " + this.repertoire.getAbsolutePath();
+			this.sendMessage(msg);
 		} else {
-			this.output.println(Constantes.CODE_REQUEST_NO_EXECUTED + " " + Constantes.MSG_NO_SUCH_FOLDER);
-			this.output.flush();
+			String msg = Constantes.CODE_REQUEST_NO_EXECUTED + " " + Constantes.MSG_NO_SUCH_FOLDER;
+			this.sendMessage(msg);
 		}
 	}
 }
